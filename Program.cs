c0588625124a1262c9e5e31e64f215503c94dedd
aleino0 <113008053+aleino0@users.noreply.github.com>
2023-09-18 11:12:56 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_sortiranje
+{
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] studentArray = {
+            new Student() { StudentID = 1, StudentName = "John", Age = 18 },
+            new Student() { StudentID = 2, StudentName = "Steve",  Age = 21 },
+            new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 },
+            new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 },
+            new Student() { StudentID = 5, StudentName = "Ron" , Age = 31 },
+            new Student() { StudentID = 6, StudentName = "Chris",  Age = 17 },
+            new Student() { StudentID = 7, StudentName = "Rob",Age = 19  },
+            };
+
+            Student[] students = new Student[10];
+
+            var ucenik = from uc in studentArray
+                         where uc.Age >12 && uc.Age<20
+                         orderby uc.Age descending
+                         select uc.StudentName+ " " + uc.Age;
+
+            foreach(var godine in ucenik)
+            {
+                Console.WriteLine(godine);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
